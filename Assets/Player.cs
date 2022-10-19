f7af3a942f7498f1c8079fe8a5303e7e8d579406
fishparc <A110082014@mail.shu.edu.tw>
2022-10-19 12:06:22 +0800

    floor and capsule

---
+++ Assets/Player.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController cc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc =GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("horizontal")
+        float v = Input.GetAxis("vertical")
+    }
+}
