f00472d353f412a107c0bd10801b0d7c99c7fc30
fishparc <A110082014@mail.shu.edu.tw>
2022-10-19 12:30:53 +0800

    2nd floor player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     CharacterController cc;
+    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +15,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxis("horizontal")
-        float v = Input.GetAxis("vertical")
+        float h = Input.GetAxis("horizontal");
+        float v = Input.GetAxis("vertical");
+        Vector3 dir = new Vector3(h,0,v);
+        
+    if (dir.magnitude > 0.1f)
+        {
+            float faceAngle = Mathf.Atan2(h, v) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0, faceAngle, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 0.3f);
+        }
+     Vector3 move=dir*speed;
+     if (!cc.isGrounded)
+        {
+            move.y = -9.8f * 100 * Time.deltaTime;
+        }
+cc.Move(dir*Time.deltaTime*speed);
     }
 }
